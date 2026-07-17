8376c3fb32d16838f0c8016a031e3b4e28ce5e4f
Chris <morenochristopher580@gmail.com>
2026-07-17 15:15:22 -0600

    Create MathUtils.cpp

---
+++ src/utils/MathUtils.cpp
@@ -0,0 +1,13 @@
+#include "MathUtils.h"
+
+// Implementation file for MathUtils
+// Most functions are inline in the header
+
+namespace MathUtils {
+
+    // Biquad coefficient calculation (to be implemented)
+    // void Biquad::updateCoefficients() {
+    //     // Full implementation here
+    // }
+
+} // namespace MathUtils
