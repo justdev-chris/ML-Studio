b62900b7f97ae993bd6d4504caf371abec5a3ee5
Chris <morenochristopher580@gmail.com>
2026-07-17 17:01:01 -0600

    Update AudioEngine with recording and FX functionality

--- src/core/engine/AudioEngine.h
+++ src/core/engine/AudioEngine.h
@@ -5,12 +5,12 @@
 #include <QVector>
 #include <QMutex>
 #include <atomic>
-
 #include "core/track/Track.h"
 #include "core/mixer/Mixer.h"
 #include "core/transport/Transport.h"
 #include "audio/io/AudioIO.h"
 #include "project/Project.h"
+#include "audio/processing/FX.h"
 
 class AudioEngine : public QObject {
     Q_OBJECT
@@ -33,7 +33,7 @@ public:
     void stop();
     void pause();
     void togglePlay();
-    void record();
+    void record(bool armed);
     bool isPlaying() const { return m_transport->isPlaying(); }
     bool isRecording() const { return m_transport->isRecording(); }
     double getCurrentPosition() const { return m_transport->getPosition(); }
@@ -53,12 +53,19 @@ public:
     Mixer* getMixer() { return m_mixer; }
     void setMasterVolume(float volume);
     float getMasterVolume() const;
+    void addMasterInsert(FX* effect);
+    void removeMasterInsert(int index);
+    void clearMasterInserts();
 
     // Project
     void setProject(Project* project);
     Project* getProject() const { return m_project; }
     void loadProject(Project* project);
     void clearProject();
+
+    // Recording
+    void setRecordPath(const QString& path);
+    QString getRecordPath() const { return m_recordPath; }
 
     // Audio processing (called by AudioIO callback)
     void processAudio(float** input, float** output, int numChannels, int numFrames);
@@ -75,6 +82,7 @@ signals:
     void projectLoaded();
     void projectCleared();
     void error(const QString& message);
+    void recordingProgress(double time);
 
 private slots:
     void onTransportPositionChanged(double seconds);
@@ -86,6 +94,7 @@ private:
     bool m_initialized = false;
     int m_sampleRate = 44100;
     int m_bufferSize = 256;
+    double m_currentTime = 0.0;
 
     Transport* m_transport;
     Mixer* m_mixer;
@@ -93,11 +102,20 @@ private:
     Project* m_project = nullptr;
 
     QVector<Track*> m_tracks;
-    mutable QMutex m_mutex;
+    QVector<FX*> m_masterInserts;
+    QMutex m_mutex;
+
+    // Recording
+    QString m_recordPath;
+    AudioClip* m_recordingClip = nullptr;
+    Track* m_recordingTrack = nullptr;
+    QVector<float> m_recordBuffer;
 
     void setupDefaultTracks();
     void connectTransportSignals();
     void clearTracks();
+    void processMasterInserts(float** buffer, int numChannels, int numFrames);
+    void handleRecording(float** input, int numChannels, int numFrames);
 };
 
 #endif // AUDIOENGINE_H
