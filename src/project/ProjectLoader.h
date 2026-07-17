3a927e945969acc6687bca6ee12a4b8e41fea419
Chris <morenochristopher580@gmail.com>
2026-07-17 17:15:01 -0600

    Update ProjectLoader.h to include project path parameters

--- src/project/ProjectLoader.h
+++ src/project/ProjectLoader.h
@@ -31,15 +31,15 @@ private:
 
     bool parseProject(const QJsonObject& json, Project* project);
     bool parseSettings(const QJsonObject& json, Project* project);
-    bool parseTracks(const QJsonArray& tracks, Project* project);
-    bool parseTrack(const QJsonObject& trackJson, Track* track);
-    bool parseAudioClip(const QJsonObject& clipJson, Clip* clip);
+    bool parseTracks(const QJsonArray& tracks, Project* project, const QString& projectPath);
+    bool parseTrack(const QJsonObject& trackJson, Track* track, const QString& projectDir);
+    bool parseAudioClip(const QJsonObject& clipJson, Clip* clip, const QString& projectDir);
     bool parseMIDIClip(const QJsonObject& clipJson, Clip* clip);
     bool parseAutomation(const QJsonObject& json, Project* project);
     bool parseMarkers(const QJsonArray& markers, Project* project);
     bool parseRegions(const QJsonArray& regions, Project* project);
 
-    QString resolvePath(const QString& relativePath);
+    QString resolvePath(const QString& relativePath, const QString& baseDir);
 };
 
 #endif // PROJECTLOADER_H
