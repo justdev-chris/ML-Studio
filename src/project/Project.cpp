26087683642047146a38e4e5b4758110ff14988a
Chris <morenochristopher580@gmail.com>
2026-07-17 18:38:35 -0600

    Refactor Project class for improved readability

--- src/project/Project.cpp
+++ src/project/Project.cpp
@@ -7,12 +7,9 @@ Project::Project(QObject* parent)
     , m_author("Unknown")
     , m_created(QDateTime::currentDateTime())
     , m_modified(QDateTime::currentDateTime())
-    , m_transport(new Transport(this)) {
-}
+    , m_transport(new Transport(this)) {}
 
-Project::~Project() {
-    clearTracks();
-}
+Project::~Project() { clearTracks(); }
 
 void Project::addTrack(Track* track) {
     if (!track) return;
@@ -32,16 +29,12 @@ void Project::removeTrack(int index) {
 
 void Project::insertTrack(int index, Track* track) {
     if (!track) return;
-    if (index < 0) index = 0;
-    if (index > m_tracks.size()) index = m_tracks.size();
     m_tracks.insert(index, track);
     emit trackAdded(track);
     setDirty(true);
 }
 
 void Project::moveTrack(int from, int to) {
-    if (from < 0 || from >= m_tracks.size()) return;
-    if (to < 0 || to >= m_tracks.size()) return;
     if (from == to) return;
     m_tracks.move(from, to);
     emit trackMoved(from, to);
@@ -54,60 +47,28 @@ Track* Project::getTrack(int index) const {
 }
 
 void Project::clearTracks() {
-    for (Track* track : m_tracks) {
-        delete track;
-    }
+    for (Track* track : m_tracks) delete track;
     m_tracks.clear();
 }
 
 void Project::setMasterVolume(float volume) {
-    if (volume < 0.0f) volume = 0.0f;
-    if (volume > 2.0f) volume = 2.0f;
-    if (m_masterVolume != volume) {
-        m_masterVolume = volume;
-        emit masterVolumeChanged(volume);
-        setDirty(true);
-    }
-}
-
-void Project::setMasterInsert(int index, const QString& plugin, const QMap<QString, float>& params) {
-    if (index < 0 || index > m_masterInserts.size()) return;
-    QMap<QString, QVariant> insert;
-    insert["plugin"] = plugin;
-    QMap<QString, QVariant> paramMap;
-    for (auto it = params.constBegin(); it != params.constEnd(); ++it) {
-        paramMap[it.key()] = it.value();
-    }
-    insert["params"] = paramMap;
-    if (index == m_masterInserts.size()) {
-        m_masterInserts.append(insert);
-    } else {
-        m_masterInserts[index] = insert;
-    }
-    setDirty(true);
-}
-
-void Project::removeMasterInsert(int index) {
-    if (index < 0 || index >= m_masterInserts.size()) return;
-    m_masterInserts.removeAt(index);
+    m_masterVolume = qBound(0.0f, volume, 2.0f);
+    emit masterVolumeChanged(m_masterVolume);
     setDirty(true);
 }
 
 void Project::addAutomationPoint(int trackIndex, const QString& parameter, double time, float value) {
-    if (trackIndex < 0 || trackIndex >= m_tracks.size()) return;
     if (time < 0) time = 0;
     m_automation[trackIndex][parameter][time] = value;
     setDirty(true);
 }
 
 void Project::removeAutomationPoints(int trackIndex, const QString& parameter) {
-    if (trackIndex < 0 || trackIndex >= m_tracks.size()) return;
     m_automation[trackIndex].remove(parameter);
     setDirty(true);
 }
 
 QMap<double, float> Project::getAutomationPoints(int trackIndex, const QString& parameter) const {
-    if (trackIndex < 0 || trackIndex >= m_tracks.size()) return QMap<double, float>();
     if (!m_automation.contains(trackIndex)) return QMap<double, float>();
     if (!m_automation[trackIndex].contains(parameter)) return QMap<double, float>();
     return m_automation[trackIndex][parameter];
